2b97f6eb85739c805565b04fd878bd133ac76012
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2020-12-29 22:50:40 +0530

    Readme file done

--- pyaadhaar/deocde.py
+++ pyaadhaar/deocde.py
@@ -5,9 +5,14 @@ import xml.etree.ElementTree as ET
 from io import BytesIO
 from PIL import Image
 import base64
+import zipfile
+import pyaadhaar
 
 
-class AdhaarSecureQr:
+class AadhaarSecureQr:
+    # This is the class for Adhaar Secure Qr code..  In this version of code the data is in encrypted format
+    # The special thing of this type of QR is that we can extract the photo of user from the data
+    # For more information check here : https://103.57.226.101/images/resource/User_manulal_QR_Code_15032019.pdf
 
     def __init__(self, base10encodedstring):
         self.base10encodedstring = base10encodedstring
@@ -48,29 +53,35 @@ class AdhaarSecureQr:
             self.data['email'] = "yes"
             self.data['mobile'] = "yes"
 
-    def decodedData(self):
+    def decodeddata(self):
+        # Will return the personal data in a dictionary format
         return self.data
 
     def signature(self):
+        # Will return the signature value
         signature = self.decompressed_array[len(
             self.decompressed_array)-256:len(self.decompressed_array)]
         return signature
 
     def signedData(self):
+        # Will return the signed data
         signeddata = self.decompressed_array[:len(self.decompressed_array)-256]
         return signeddata
 
     def isMobileNoRegistered(self):
+        # Will return True if mobile number is registered
         if int(self.data['email_mobile_status']) == 3 or int(self.data['email_mobile_status']) == 1:
             return True
         return False
 
     def isEmailRegistered(self):
+        # Will return True if email id is registered
         if int(self.data['email_mobile_status']) == 3 or int(self.data['email_mobile_status']) == 2:
             return True
         return False
 
     def sha256hashOfEMail(self):
+        # Will return the hash of the email id
         tmp = ""
         if int(self.data['email_mobile_status']) == 3:
             tmp = self.decompressed_array[len(
@@ -81,6 +92,7 @@ class AdhaarSecureQr:
         return tmp
 
     def sha256hashOfMobileNumber(self):
+        # Will return the hash of mobile number
         tmp = ""
         if int(self.data['email_mobile_status']) == 3 or int(self.data['email_mobile_status']) == 1:
             tmp = self.decompressed_array[len(
@@ -88,6 +100,7 @@ class AdhaarSecureQr:
         return tmp
 
     def image(self):
+        # Will return the image stream to be used in another function
         if int(self.data['email_mobile_status']) == 3:
             return Image.open(BytesIO(self.decompressed_array[self.delimeter[15]+1:len(self.decompressed_array)-256-32-32]))
         elif int(self.data['email_mobile_status']) == 2 or int(self.data['email_mobile_status']) == 1:
@@ -98,11 +111,13 @@ class AdhaarSecureQr:
             return None
 
     def saveimage(self, filename):
+        # Will save the image of user
         image = self.image()
         image.save(filename)
 
     def verifyEmail(self, emailid):
-        generated_sha_mail = SHAGenerator(
+        # Will return True if emailid match with the given email id
+        generated_sha_mail = pyaadhaar.utils.SHAGenerator(
             emailid, self.data['adhaar_last_digit'])
         if generated_sha_mail == self.sha256hashOfEMail():
             return True
@@ -110,7 +125,8 @@ class AdhaarSecureQr:
             return False
 
     def verifyMobileNumber(self, mobileno):
-        generated_sha_mobile = SHAGenerator(
+        # Will return True if mobileno match with the given mobile no
+        generated_sha_mobile = pyaadhaar.utils.SHAGenerator(
             mobileno, self.data['adhaar_last_digit'])
         if generated_sha_mobile == self.sha256hashOfMobileNumber():
             return True
@@ -118,7 +134,9 @@ class AdhaarSecureQr:
             return False
 
 
-class AdhaarOldQr:
+class AadhaarOldQr:
+    # This is the class for Adhaar Normal Qr code..  In this version of code the data is in XML v1.0 format
+    # For more information check here : https://103.57.226.101/images/resource/User_manulal_QR_Code_15032019.pdf
 
     def __init__(self, qrdata):
         self.qrdata = qrdata
@@ -127,16 +145,26 @@ class AdhaarOldQr:
         self.data = self.parsedxml.attrib
 
     def decodeddata(self):
+        # Will return the decoded datas inn dictionary format
         return self.data
 
 
-class AdhaarOfflineXML:
-    def __init__(self, filename, passcode):
+class AadhaarOfflineXML:
+
+    # This is the class for Adhaar Offline XML
+    # The special thing of Offline XML is that we can extract the high quality photo of user from the data
+    # For more information check here : https://103.57.226.101/images/resource/User_manulal_QR_Code_15032019.pdf
+
+    def __init__(self, file, passcode):
+        # Need to pass the zip file and passcode/sharecode to this function
         self.passcode = passcode
         self.data = {}
-        self.filename = filename
-        parsedxml = ET.parse(filename, parser=ET.XMLParser(encoding='utf-8'))
-        self.root = parsedxml.getroot()
+        zf = zipfile.ZipFile(file, 'r')
+        zf.setpassword(str(self.passcode).encode('utf-8'))
+        filedata = zf.open(zf.namelist()[0]).read()
+        parsedxml = ET.fromstring(
+            filedata, parser=ET.XMLParser(encoding="utf-8"))
+        self.root = parsedxml
 
         self.hashofmobile = self.root[0][0].attrib['m']
         self.hashofemail = self.root[0][0].attrib['e']
@@ -182,38 +210,47 @@ class AdhaarOfflineXML:
             self.data['mobile'] = "yes"
 
     def decodeddata(self):
+        # Will return data in dictionary format
         return self.data
 
     def signature(self):
+        # Will return the signature
         return self.root[1][1].text
 
     def isMobileNoRegistered(self):
+        # Will return True if mobile number is registered
         if int(self.data['email_mobile_status']) == 3 or int(self.data['email_mobile_status']) == 1:
             return True
         return False
 
     def isEmailRegistered(self):
+        # Will return True if email id is registered
         if int(self.data['email_mobile_status']) == 3 or int(self.data['email_mobile_status']) == 2:
             return True
         return False
 
     def sha256hashOfEMail(self):
+        # Will return the hash of the email id
         return self.hashofemail
 
     def sha256hashOfMobileNumber(self):
+        # Will return the hash of mobile number
         return self.hashofmobile
 
     def image(self):
+        # Will return the image stream to be used in another function
         img = self.root[0][2].text
         img = Image.open(BytesIO(base64.b64decode(img)))
         return img
 
     def saveimage(self, filename):
+        # Will save the image of user
         image = self.image()
         image.save(filename)
 
     def verifyEmail(self, emailid):
-        generated_sha_mail = SHAGenerator(
+        # Will return True if emailid match with the given email id
+        generated_sha_mail = pyaadhaar.utils.SHAGenerator(
             str(emailid)+str(self.passcode), self.data['adhaar_last_digit'])
         if generated_sha_mail == self.sha256hashOfEMail():
             return True
@@ -221,7 +258,8 @@ class AdhaarOfflineXML:
             return False
 
     def verifyMobileNumber(self, mobileno):
-        generated_sha_mobile = SHAGenerator(
+        # Will return True if mobileno match with the given mobile no
+        generated_sha_mobile = pyaadhaar.utils.SHAGenerator(
             str(mobileno)+str(self.passcode), self.data['adhaar_last_digit'])
         if generated_sha_mobile == self.sha256hashOfMobileNumber():
             return True
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pyadhaar',
+    name='pyaadhaar',
     packages=find_packages(),
     version='0.1.0',
-    description='MY FIRST',
+    description='This library is built to ease the process of decoding aadhaar QR codes and XML. It supprts old aadhaar QR codes , newly released Secure aadhaar QR codes and also Offline e-KYC XML. This library also can decode QR codes with Opncv. This library bundled with all the features to verify user\'s Email Id and Mobile Number & also to extract the photo of user. ',
     author='Tanmoy Sarkar',
+    author_email='ts741127@gmail.com',
     license='MIT',
 )
-
