c5ef4318abc4fa341eb486f67ff6ecafe648e6a4
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-07-26 11:34:55 +0530

    2.0.2 release version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.MD", "r", encoding="utf-8") as fh:
 setup(
     name='pyaadhaar',
     packages=find_packages(),
-    version='2.0.1',
+    version='2.0.2',
     description='This library is built to ease the process of decoding aadhaar QR codes and XML. It supprts old aadhaar QR codes , newly released Secure aadhaar QR codes and also Offline e-KYC XML. This library also can decode QR codes with Opncv. This library bundled with all the features to verify user\'s Email Id and Mobile Number & also to extract the photo of user. ',
     long_description=long_description,
     long_description_content_type="text/markdown",
